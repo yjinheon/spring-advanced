f495b657b0c094d181fb7a5247f494edcbd13e97
jinheonyoon <yjinheon@gmail.com>
2023-12-06 22:49:40 +0900

    HelloTraceV1

---
+++ src/main/java/datamind/advanced/trace/TraceStatus.java
@@ -0,0 +1,29 @@
+package datamind.advanced.trace;
+
+import lombok.Getter;
+
+// 로그 시작할 때 상태 정보를 저장하는 클래스
+public class TraceStatus {
+
+    private TraceId traceId;
+    private long startTimeMs; // 로그 시작 시간
+    private String message;
+
+    public TraceStatus(TraceId traceId, long startTime, String message) {
+        this.traceId = traceId;
+        this.startTimeMs = startTime;
+        this.message = message;
+    }
+
+    public TraceId getTraceId() {
+        return traceId;
+    }
+
+    public long getStartTimeMs() {
+        return startTimeMs;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
